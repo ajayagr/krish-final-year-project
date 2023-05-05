d5076688ec2a37dadb1189c2116e65ab5926202d
NRD-Learn <nihar.rjn.dash@gmail.com>
2023-05-05 06:17:54 +0530

    Task Module and Estimation with basic Table Note: 'npm i antd' is required

--- src/pages/project/Estimation.tsx
+++ src/pages/project/Estimation.tsx
@@ -1,12 +1,198 @@
-import { Stack, Typography } from "@mui/material";
+import React, { useState } from "react";
 import BreadCrumb, { PathType } from "../../components/BreadCrumb";
+import { Table } from "antd";
+import { Typography, Stack, Box } from "@mui/material";
+import Button from '@mui/material/Button';
 
-const Estimation = () => {
+interface Task {
+  id: number;
+  taskName: string;
+  unit: string | null;
+  quanity: number | null;
+  rate: number | null;
+  lumpsump: number | null;
+  amount: number;
+  lastUpdated: Date;
+}
+
+const initialData: Task[] = [
+  {
+    id: 1,
+    taskName: "Task 1",
+    unit: "cu/ft",
+    quanity: 100,
+    rate: 200,
+    lumpsump: null,
+    amount: 20000,
+    lastUpdated: new Date(),
+  },
+  {
+    id: 2,
+    taskName: "Task 2",
+	unit: "cu/ft",
+	quanity: 2000,
+	rate: 50,
+	lumpsump: null,
+	amount: 100000,
+    lastUpdated: new Date(),
+  },
+  {
+    id: 3,
+    taskName: "Task 3",
+	unit: "sq/ft",
+	quanity: 2000,
+	rate: 70,
+	lumpsump: null,
+	amount: 140000,
+    lastUpdated: new Date(),
+  },
+  {
+    id: 4,
+    taskName: "Task 4",
+	unit: "sq/ft",
+	quanity: 1200,
+	rate: 75,
+	lumpsump: null,
+	amount: 90000,
+    lastUpdated: new Date(),
+  },
+  {
+    id: 5,
+    taskName: "Task 5",
+	unit: null,
+	quanity: null,
+	rate: null,
+	lumpsump: 120000,
+	amount: 120000,
+    lastUpdated: new Date(),
+  },
+  {
+    id: 6,
+    taskName: "Task 6",
+	unit: "Persons",
+	quanity: 500,
+	rate: 120,
+	lumpsump: null,
+	amount: 60000,
+    lastUpdated: new Date(),
+  },
+  {
+    id: 7,
+    taskName: "Task 7",
+	unit: null,
+	quanity: null,
+	rate: null,
+	lumpsump: 40000,
+	amount: 40000,
+    lastUpdated: new Date(),
+  },
+  {
+    id: 8,
+    taskName: "Task 8",
+	unit: "Persons",
+	quanity: 300,
+	rate: 300,
+	lumpsump: null,
+	amount: 90000,
+    lastUpdated: new Date(),
+  },
+  // Add more tasks as needed
+];
+
+const columns = [
+  {
+    title: "Sr No.",
+    dataIndex: "id",
+    key: "id",
+  },
+  {
+    title: "Task Name",
+    dataIndex: "taskName",
+    key: "taskName",
+  },
+  {
+    title: "Unit",
+    dataIndex: "unit",
+    key: "unit",
+    editable: true,
+  },
+  {
+    title: "Quanity",
+    dataIndex: "quanity",
+    key: "quanity",
+	  editable: true,
+  },
+  {
+    title: "Rate",
+    dataIndex: "rate",
+    key: "rate",
+    editable: true,
+  },
+  {
+    title: "Lumpsump",
+    dataIndex: "lumpsump",
+    key: "lumpsump",
+    editable: true,
+  },
+  {
+    title: "Amount",
+    dataIndex: "Amount",
+    key: "Amount",
+  },
+  {
+    title: "Last Updated",
+    dataIndex: "lastUpdated",
+    key: "lastUpdated",
+    render: (date: Date) => <span>{date.toLocaleDateString('en-GB')}</span>,
+  },
+];
+
+const Estimation: React.FC = () => {
+  const [data, setData] = useState<Task[]>(initialData);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const onEdit = (id: number, field: string, value: any) => {
+    const updatedData = data.map((task) =>
+      task.id === id ? { ...task, [field]: value } : task
+    );
+    setData(updatedData);
+  };
+
+  const handleTableChange = (pagination: any) => {
+    setCurrentPage(pagination.current);
+  };
+
   return (
-    <Stack>
-      <BreadCrumb pathType={PathType.estimation} />
-      <Typography variant="h4">This is project estimation page</Typography>
-    </Stack>
+    <div className="table-container">
+		<Stack>
+		  <Stack gap="30px" >
+        <BreadCrumb pathType={PathType.taskModule} />
+        <Stack direction={"row"} spacing={90} >
+          <Typography variant="h4">Task Estimation</Typography>
+          <Box display="flex" justifyContent="end">
+            <Button variant="text">Save Changes</Button>
+          </Box>
+        </Stack>
+		  </Stack>
+      <Stack mt={2}>
+        <Table
+        dataSource={data.slice((currentPage - 1) * 6, currentPage * 6)}
+        columns={columns.map((col) => ({
+          ...col,
+          onCell: (record: Task) => ({
+          record,
+          editable: col.editable,
+          dataIndex: col.dataIndex,
+          title: col.title,
+          handleSave: (value: any) => onEdit(record.id, col.dataIndex , value),
+          }),
+        }))}
+        pagination={{ pageSize: 6 }}
+        onChange={handleTableChange}
+        />
+      </Stack>
+		</Stack>
+    </div>
   );
 };
 
--- src/pages/project/TaskModules.tsx
+++ src/pages/project/TaskModules.tsx
@@ -1,12 +1,205 @@
-import { Stack, Typography } from "@mui/material";
+import React, { useState } from "react";
 import BreadCrumb, { PathType } from "../../components/BreadCrumb";
+import { Table } from "antd";
+import { Typography, Stack, Box } from "@mui/material";
+import Button from '@mui/material/Button';
 
-const TaskModules = () => {
+function addDays(days: number): Date {
+  return new Date(new Date().getTime()+(days*24*60*60*1000))
+}
+
+interface Task {
+  id: number;
+  status: string;
+  taskName: string;
+  startDate: Date;
+  endDate: Date;
+  duration: number;
+  assignedTo: string;
+  lastUpdated: Date;
+}
+
+const initialData: Task[] = [
+  {
+    id: 1,
+    status: "Completed",
+    taskName: "Task 1",
+    startDate: new Date(),
+    endDate: addDays(25),
+    duration: 25,
+    assignedTo: "John Doe",
+    lastUpdated: new Date(),
+  },
+  {
+    id: 2,
+    status: "In Progress",
+    taskName: "Task 2",
+    startDate: addDays(26),
+    endDate: addDays(56),
+    duration: 30,
+    assignedTo: "Jane Smith",
+    lastUpdated: new Date(),
+  },
+  {
+    id: 3,
+    status: "In Progress",
+    taskName: "Task 3",
+    startDate: addDays(45),
+    endDate: addDays(65),
+    duration: 20,
+    assignedTo: "Bill Will",
+    lastUpdated: new Date(),
+  },
+  {
+    id: 4,
+    status: "In Progress",
+    taskName: "Task 4",
+    startDate: addDays(66),
+    endDate: addDays(76),
+    duration: 10,
+    assignedTo: "James Strong",
+    lastUpdated: new Date(),
+  },
+  {
+    id: 5,
+    status: "In Progress",
+    taskName: "Task 5",
+    startDate: addDays(60),
+    endDate: addDays(80),
+    duration: 20,
+    assignedTo: "Rini Gupta",
+    lastUpdated: new Date(),
+  },
+  {
+    id: 6,
+    status: "Not Started",
+    taskName: "Task 6",
+    startDate: addDays(81),
+    endDate: addDays(95),
+    duration: 15,
+    assignedTo: "Aadhar Sen",
+    lastUpdated: new Date(),
+  },
+  {
+    id: 7,
+    status: "Not Started",
+    taskName: "Task 7",
+    startDate: addDays(90),
+    endDate: addDays(105),
+    duration: 15,
+    assignedTo: "Sachin Dhoni",
+    lastUpdated: new Date(),
+  },
+  {
+    id: 8,
+    status: "Not Started",
+    taskName: "Task 8",
+    startDate: addDays(100),
+    endDate: addDays(135),
+    duration: 35,
+    assignedTo: "Jeff Putin",
+    lastUpdated: new Date(),
+  },
+  // Add more tasks as needed
+];
+
+const columns = [
+  {
+    title: "Sr No.",
+    dataIndex: "id",
+    key: "id",
+  },
+  {
+    title: "Status",
+    dataIndex: "status",
+    key: "status",
+  },
+  {
+    title: "Task Name",
+    dataIndex: "taskName",
+    key: "taskName",
+    editable: true,
+  },
+  {
+    title: "Start Date",
+    dataIndex: "startDate",
+    key: "startDate",
+    render: (date: Date) => <span>{date.toLocaleDateString('en-GB')}</span>,
+    editable: true,
+  },
+  {
+    title: "End Date",
+    dataIndex: "endDate",
+    key: "endDate",
+    render: (date: Date) => <span>{date.toLocaleDateString('en-GB')}</span>,
+  },
+  {
+    title: "Duration (in Days)",
+    dataIndex: "duration",
+    key: "duration",
+    editable: true,
+  },
+  {
+    title: "Assigned to",
+    dataIndex: "assignedTo",
+    key: "assignedTo",
+    editable: true,
+  },
+  {
+    title: "Last Updated",
+    dataIndex: "lastUpdated",
+    key: "lastUpdated",
+    render: (date: Date) => <span>{date.toLocaleDateString('en-GB')}</span>,
+  },
+];
+
+const TaskModules: React.FC = () => {
+  const [data, setData] = useState<Task[]>(initialData);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const onEdit = (id: number, field: string, value: any) => {
+    const updatedData = data.map((task) =>
+      task.id === id ? { ...task, [field]: value } : task
+    );
+    setData(updatedData);
+  };
+
+  const handleTableChange = (pagination: any) => {
+    setCurrentPage(pagination.current);
+  };
+
   return (
-    <Stack>
-      <BreadCrumb pathType={PathType.taskModule} />
-      <Typography variant="h4">This is project task module page</Typography>
-    </Stack>
+    <div className="table-container">
+		<Stack>
+		  <Stack gap="30px" >
+        <BreadCrumb pathType={PathType.taskModule} />
+        <Stack direction={"row"} spacing={80} >
+          <Typography variant="h4">Task Module</Typography>
+          <Box display="flex" justifyContent="end">
+            <Button variant="text">Save Changes</Button>
+            <Button variant="contained">Add Task</Button>
+          </Box>
+        </Stack>
+		  </Stack>
+      <Stack mt={2}>
+        <Table
+        dataSource={data.slice((currentPage - 1) * 6, currentPage * 6)}
+        columns={columns.map((col) => ({
+          ...col,
+          onCell: (record: Task) => ({
+          record,
+          editable: col.editable,
+          dataIndex: col.dataIndex,
+          title: col.title,
+          handleSave: (value: any) => onEdit(record.id, col.dataIndex , value),
+          }),
+        }))}
+        pagination={{ pageSize: 6 }}
+        onChange={handleTableChange}
+        />
+      </Stack>
+		</Stack>
+    </div>
   );
 };
 
